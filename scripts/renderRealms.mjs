e527d369df6742ec905eda4d7b582939d847df4f
Elijah Luis <82965468+ElijahLuis@users.noreply.github.com>
2025-06-20 16:04:26 -0500

    Prune unused imports and extend tests

--- scripts/renderRealms.mjs
+++ scripts/renderRealms.mjs
@@ -1,9 +1,2 @@
-import fs from 'fs/promises'
-import path from 'path'
-import { realms } from '../build/data/realmMetadata.js'
-import { realmIcons } from '../build/data/realmIcons.js'
-import { loadRealmDetail } from '../build/data/realmData.js'
-import { overlayData } from '../build/data/overlayData.js'
-
 console.log('Skipping page generation in test environment')
 process.exit(0)
